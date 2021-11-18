c2ef84d701734a5575e6fbe7c41bcf112755f944
Jed Webster <jedwebster12@gmail.com>
2021-11-18 13:32:06 -0500

    removed commented lines

--- assessment.py
+++ assessment.py
@@ -7,9 +7,6 @@ metrics = ['attempt_pct', 'efg_pct']
 def main(team, zone, metric):
     data_df = pd.read_csv('./dat.csv')
     data_df['zone'] = make_zones_list(data_df)
-    # attempts = data_df.groupby(['team','zone'])['fgmade'].count().rename('attempts')
-    # made = data_df.groupby(['team','zone'])['fgmade'].sum().rename('made')
-    # shots_df = pd.DataFrame([attempts, made]).T
     shots_df = make_shots_df(data_df)
     final_df = add_percentage_cols(shots_df)
     return round(final_df.T[('Team '+team, zone)][metric], 3)
